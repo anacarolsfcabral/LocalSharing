3015ace9efe1d21075da4cd0d9a7b80f49ebc1e2
anacarolsfcabral <aninha_carolcabral@hotmail.com>
2015-03-16 16:16:22 -0300

    Initial Commit

---
+++ LocalSharing/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LocalSharing
+//
+//  Created by Ana Carolina Cabral on 16/03/15.
+//  Copyright (c) 2015 Ana Carolina Cabral. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LocalSharing/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  LocalSharing
+//
+//  Created by Ana Carolina Cabral on 16/03/15.
+//  Copyright (c) 2015 Ana Carolina Cabral. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
