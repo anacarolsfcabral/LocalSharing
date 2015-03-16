038924647dda6821d65a6ee0f007b51e108fb9f8
anacarolsfcabral <aninha_carolcabral@hotmail.com>
2015-03-16 18:39:39 -0300

    FirstScreen with Facebook login

---
+++ LocalSharing/FirstScreen.swift
@@ -0,0 +1,51 @@
+//
+//  FirstScreen.swift
+//  LocalSharing
+//
+//  Created by Ana Carolina Cabral on 16/03/15.
+//  Copyright (c) 2015 Ana Carolina Cabral. All rights reserved.
+//
+
+import UIKit
+
+class FirstScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginButton(sender: UIButton) {
+        PFFacebookUtils.logInWithPermissions(["public_profile"], {
+            (user: PFUser!, error: NSError!) -> Void in
+            if let user = user {
+                if user.isNew {
+                    println("User signed up and logged in through Facebook!")
+                } else {
+                    println("User logged in through Facebook!")
+                }
+            } else {
+                println("Uh oh. The user cancelled the Facebook login.")
+            }
+        })
+        
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
